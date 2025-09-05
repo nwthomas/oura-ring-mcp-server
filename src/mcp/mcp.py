5c1a36ca433f039ad61b701583bfcf87458ae81b
Nathan Thomas <nwthomas@me.com>
2025-09-04 23:04:09 -0700

    Add 2 more routes

--- src/mcp/mcp.py
+++ src/mcp/mcp.py
@@ -1,9 +1,23 @@
 from mcp.server.fastmcp import FastMCP
 from .constants import SERVER_NAME
 from typing import Any
-from ..oura_ring.routes import get_oura_ring_daily_cardiovascular_age
+from ..oura_ring.routes import get_oura_ring_daily_cardiovascular_age, get_oura_ring_multiple_daily_activity, get_oura_ring_multiple_daily_readiness
 
 mcp = FastMCP(name=SERVER_NAME)
+
+@mcp.tool()
+async def get_multiple_daily_activity(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
+    """Get the multiple daily activity for a given date range.
+    
+    Args:
+        start_date: The start date of the date range. Must be in YYYY-MM-DD format.
+        end_date: The end date of the date range. Must be in YYYY-MM-DD format.
+        next_token: The next token to use for pagination from a previous request
+        
+    Returns:
+        The multiple daily activity for each day in a given date range.
+    """
+    return await get_oura_ring_multiple_daily_activity(start_date, end_date, next_token)
 
 @mcp.tool()
 async def get_daily_cardiovascular_age(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
@@ -15,10 +29,23 @@ async def get_daily_cardiovascular_age(start_date: str, end_date: str, next_toke
         next_token: The next token to use for pagination from a previous request
         
     Returns:
-        The daily cardiovascular age for the given date range.
+        The daily cardiovascular age for each day in a given date range.
     """
     return await get_oura_ring_daily_cardiovascular_age(start_date, end_date, next_token)
+
+@mcp.tool()
+async def get_multiple_daily_readiness(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
+    """Get the multiple daily readiness for a given date range.
     
+    Args:
+        start_date: The start date of the date range. Must be in YYYY-MM-DD format.
+        end_date: The end date of the date range. Must be in YYYY-MM-DD format.
+        next_token: The next token to use for pagination from a previous request
+        
+    Returns:
+        The multiple daily readiness for each day in a given date range.
+    """
+    return await get_oura_ring_multiple_daily_readiness(start_date, end_date, next_token)
 
 def start_server():
     print(f"Starting {SERVER_NAME}")
--- src/oura_ring/routes.py
+++ src/oura_ring/routes.py
@@ -2,21 +2,53 @@ from .constants import OURA_RING_API_BASE, OURA_RING_PERSONAL_ACCESS_TOKEN, SERV
 from httpx import AsyncClient
 from typing import Any
 
-async def make_oura_ring_request(url: str) -> dict[str, Any] | None:
-    """Make a request to the Oura Ring API with proper error handling."""
-    headers = {
+def build_oura_ring_request_headers() -> dict[str, str]:
+    return {
         "Accept": "application/json",
         "Authorization": f"Bearer {OURA_RING_PERSONAL_ACCESS_TOKEN}",
         "User-Agent": SERVER_USER_AGENT,
     }
+
+def build_oura_ring_request_params(params: dict[str, Any]) -> dict[str, Any]:
+    """This mostly screens out any parameters that have None as their values as this causes
+    the Oura Ring API to throw a 400 error.
+    """
+    return {k: v for k, v in params.items() if v is not None}
+
+async def make_oura_ring_request(url: str, params: dict[str, Any] | None = None) -> dict[str, Any] | None:
+    """Make a request to the Oura Ring API with proper error handling."""
+    headers = build_oura_ring_request_headers()
     async with AsyncClient() as client:
         try:
-            response = await client.get(url, headers=headers, timeout=SERVER_TIMEOUT_SECONDS)
+            response = await client.get(url, headers=headers, params=params, timeout=SERVER_TIMEOUT_SECONDS)
             response.raise_for_status()
             return response.json()
         except Exception:
             return None
+
+async def get_oura_ring_multiple_daily_activity(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
+    url = f"{OURA_RING_API_BASE}/v2/usercollection/daily_activity"
+    params = build_oura_ring_request_params({
+        "start_date": start_date,
+        "end_date": end_date,
+        "next_token": next_token,
+    })
+    return await make_oura_ring_request(url, params)
         
 async def get_oura_ring_daily_cardiovascular_age(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
-    url = f"{OURA_RING_API_BASE}/v2/usercollection/daily_cardiovascular_age?start_date={start_date}&end_date={end_date}"
-    return await make_oura_ring_request(url)
+    url = f"{OURA_RING_API_BASE}/v2/usercollection/daily_cardiovascular_age"
+    params = build_oura_ring_request_params({
+        "start_date": start_date,
+        "end_date": end_date,
+        "next_token": next_token,
+    })
+    return await make_oura_ring_request(url, params)
+
+async def get_oura_ring_multiple_daily_readiness(start_date: str, end_date: str, next_token: str | None = None) -> dict[str, Any]:
+    url = f"{OURA_RING_API_BASE}/v2/usercollection/daily_readiness"
+    params = build_oura_ring_request_params({
+        "start_date": start_date,
+        "end_date": end_date,
+        "next_token": next_token,
+    })
+    return await make_oura_ring_request(url, params)
