9afb164cdfaa56aa4587a6be4743bc6d9fe86e28
Nathan Thomas <nwthomas@me.com>
2025-09-07 20:55:19 -0700

    Clean up tests

--- tests/oura_ring/test_utils.py
+++ tests/oura_ring/test_utils.py
@@ -9,7 +9,13 @@ def test_build_oura_ring_request_headers() -> None:
     assert "Authorization" in headers
     assert "User-Agent" in headers
 
-def test_build_oura_ring_request_params() -> None:
+def test_build_oura_ring_request_params_no_filtering() -> None:
+    params = {"start_date": "2025-09-04", "end_date": "2025-09-04"}
+    filtered_params = build_oura_ring_request_params(params)
+    assert "start_date" in filtered_params
+    assert "end_date" in filtered_params
+
+def test_build_oura_ring_request_params_filter_none_values() -> None:
     params = {"start_date": "2025-09-04", "end_date": None}
     filtered_params = build_oura_ring_request_params(params)
     assert "start_date" in filtered_params
@@ -17,14 +23,20 @@ def test_build_oura_ring_request_params() -> None:
 
 @pytest.mark.asyncio
 async def test_make_oura_ring_request(mocker: pytest_mock.MockerFixture) -> None:
-    url = "https://api.oura.com/v2/usercollection/daily_activity"
-    params = {"start_date": "2025-09-04", "end_date": None}
-    mock_response = mocker.Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {"data": "mocked_data"}
+    url = "https://api.ouraring.com/v2/usercollection/daily_activity"
+    params = {"start_date": "2025-09-04", "end_date": "2025-09-04"}
     async with AsyncClient() as client:
-        mocker.patch.object(client, 'get', return_value=mock_response)
-        response = await client.get(url, params=params)
+        response = await make_oura_ring_request(url, params)
         assert response is not None
-        assert "data" in response.json()
-        assert response.status_code == 200
+        assert "data" in response
+
+@pytest.mark.asyncio
+async def test_make_oura_ring_request_error(mocker: pytest_mock.MockerFixture) -> None:
+    url = "https://api.ouraring.com/v2/usercollection/daily_activity"
+    params = {"start_date": "2025-09-05", "end_date": "2025-09-04"}
+    async with AsyncClient() as client:
+        try:
+            await make_oura_ring_request(url, params)
+            assert False
+        except Exception:
+            assert True
